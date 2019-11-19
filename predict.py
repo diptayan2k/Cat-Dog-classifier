0e17cd37b7a4ff1a388c491038d4f46f880fbf53
DIPTAYAN BISWAS <diptayan2k@gmail.com>
2019-11-19 11:03:48 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -22,8 +22,8 @@ def load_image(img_path, show=True):
 '''for img in glob.glob("test*.jpg"):
     new_image = load_image(img)
     pred = classifier.predict(new_image)
-    if pred<.5 : print("chat")
-    else : print("not chat")'''
+    if pred<.5 : print("cat")
+    else : print("dog")'''
 
 new_image = load_image("image_path.jpg")
 pred= classifier.predict(new_image)
